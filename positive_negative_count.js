d48c89939f197f503bf9a54b426e1eb751085a4c
mradzvilavicius <mantas.radzvilavicius@bazaarvoice.com>
2019-07-03 13:17:18 -0500

    pataisymas2

--- positive_negative_count.js
+++ positive_negative_count.js
@@ -2,16 +2,14 @@ function countPositivesSumNegatives(input) {
   let newArray = [];
 	let positiveEl = 0;
 	let negativeEl = 0;
-	if (input !== null && input.length > 0){
+	if (input !== null && input.length > 0) {
 		input.forEach(function(el) {
 	if (el > 0) {
 		positiveEl++
 	} else if (el < 0) {
 		negativeEl += el
-	} else {
-		newArray = []
-	}
-}
+	} 
+ }
 )
 newArray.push(positiveEl);
 newArray.push(negativeEl);
